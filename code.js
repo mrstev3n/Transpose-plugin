09dc4bdb0fb63994f4936349d12316f5b5b96020
StevenKejjad <60428738+StevenKejjad@users.noreply.github.com>
2021-08-15 22:20:29 +0100

    transpose W & H

---
+++ code.js
@@ -0,0 +1,29 @@
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = (msg) => {
+    // console.log(,'-----A------')
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === 'create-rectangles') {
+        const nodes = [];
+        const rr = figma.currentPage.selection[0];
+        // rr.rotation = msg.degree
+        const width = rr.width;
+        const height = rr.height;
+        rr.resize(height, width);
+        // figma.currentPage.selection = nodes;
+        // figma.viewport.scrollAndZoomIntoView(nodes);
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+};
