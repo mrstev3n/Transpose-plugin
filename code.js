10bccc6467b5365215e176954e5ad6419fe26531
StevenKejjad <stevenkejjad@gmail.com>
2021-08-16 08:56:05 +0100

    Transpose for one layer at a time

--- code.js
+++ code.js
@@ -1,29 +1,38 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-figma.loadFontAsync({ family: "Roboto", style: "Regular" });
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = (msg) => {
-    // console.log(,'-----A------')
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        const rr = figma.currentPage.selection[0];
-        // rr.rotation = msg.degree
-        const width = rr.width;
-        const height = rr.height;
-        rr.resize(height, width);
-        // figma.currentPage.selection = nodes;
-        // figma.viewport.scrollAndZoomIntoView(nodes);
+    const nodes = [];
+    const form = figma.currentPage.selection[0];
+
+    let { selection } = figma.currentPage;
+
+    function SelectionSize() {
+
+    const width = form.width;
+    const height = form.height;
+    form.resize(height, width);
+
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
+
+    if (!(selection.length > 0)) {
+
+        // figma.notify('Please select at least one node');
+
+        figma.closePlugin('Please select at least one node');
+
+      } 
+      
+      else {
+
+        nodes.push(SelectionSize());
+
+        // Flip W & H
+
+        /*
+            const width = form.width;
+            const height = form.height;
+            form.resize(height, width);     
+        */
+
+      }
+    
     figma.closePlugin();
-};
+
+
